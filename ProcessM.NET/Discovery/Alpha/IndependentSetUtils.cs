ef7cdabe1207b44a5d985a7d993dd2facb11b923
Radek Václavek <456268@muni.cz>
2021-07-08 17:07:09 +0200

    IndependentSetUtils  - heavy decomposition of both public fuctions to improve readibility  - changed how independent sets are calculated to be more consistent  - added initialization of sets A, B
    TraceGenerator
     - added a simple recursive generator of activity traces from given Petri net.

--- ProcessM.NET/Discovery/Alpha/IndependentSetUtils.cs
+++ ProcessM.NET/Discovery/Alpha/IndependentSetUtils.cs
@@ -10,6 +10,64 @@ namespace ProcessM.NET.Discovery.Alpha
     /// </summary>
     static class IndependentSetUtils
     {
+
+        /// <summary>
+        /// Checks whether all activities in given activity subset are independent of each other.
+        /// </summary>
+        /// <param name="subset">Subset of activities.</param>
+        /// <param name="relations">Examined relation matrix (causal footprint).</param>
+        /// <param name="activities">Activities in given relation matrix.</param>
+        /// <returns>True if all activities in given subset are independent of each other. Otherwise returns false</returns>
+        private static bool AllIndependent(HashSet<string> subset, Relation[,] relations, List<string> activities)
+        {
+            foreach (string activityA in subset)
+            {
+                foreach (string activityB in subset)
+                {
+                    if (relations[activities.IndexOf(activityA), activities.IndexOf(activityB)] != Relation.Independency)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Generates an independent power set (set of subsets in which all activites are independent of each other)
+        /// from given set using binary masking method. Inspired by:
+        /// https://stackoverflow.com/questions/19890781/creating-a-power-set-of-a-sequence
+        /// </summary>
+        /// <param name="originalSet">Set of strings from which power set is to be generated.</param>
+        /// <param name="relations">Examined relation matrix (causal footprint).</param>
+        /// <param name="activities">Activities in given relation matrix.</param>
+        /// <returns>Power set of originalSet</returns>
+        private static HashSet<HashSet<string>> GenerateIndependentPowerSet(HashSet<string> originalSet,
+            Relation[,] relations, List<string> activities)
+        {
+            List<string> listSet = new List<string>(originalSet);
+            int powerSetCount = 1 << originalSet.Count;
+            var powerSet = new HashSet<HashSet<string>>();
+
+            for (int setMask = 1; setMask < powerSetCount; ++setMask)
+            {
+                var subset = new HashSet<string>();
+                for (int i = 0; i < originalSet.Count; ++i)
+                {
+                    if ((setMask & (1 << i)) > 0)
+                    {
+                        subset.Add(listSet[i]);
+                    }
+                }
+
+                if (AllIndependent(subset, relations, activities))
+                {
+                    powerSet.Add(subset);
+                }
+            }
+            return powerSet;
+        }
+
         /// <summary>
         /// Finds all independent sets of activities (A never comes before B and B never comes before A) in given relation matrix.
         /// </summary>
@@ -19,74 +77,96 @@ namespace ProcessM.NET.Discovery.Alpha
         public static HashSet<HashSet<string>> FindIndependentSets(Relation[,] relations, List<string> activities)
         {
             int matSize = activities.Count;
-            var independentSets = new HashSet<HashSet<string>>();
+            var independentSets = new HashSet<HashSet<string>>(HashSet<string>.CreateSetComparer());
 
-            for (int i = 0; i < matSize; i++)
+            for (int i = 0; i < matSize; ++i)
             {
-                if (relations[i, i] == Relation.Independency)
+                var nset = new HashSet<string>();
+                for (int j = 0; j < matSize; ++j)
                 {
-                    var nset = new HashSet<string>() { activities[i] };
-                    independentSets.Add(new HashSet<string>(nset));
-
-                    for (int j = 0; j < matSize; j++)
+                    if (relations[i, j] == Relation.Independency)
                     {
-                        nset = new HashSet<string>() { activities[i] };
-                        for (int k = j; k < matSize; k++)
-                        {
-                            bool allIndependent = true;
-                            if (relations[i, k] == Relation.Independency && i != k)
-                            {
-                                foreach (string act in nset)
-                                {
-                                    int m = activities.FindIndex(a => a == act);
-                                    if (relations[m, k] != Relation.Independency)
-                                    {
-                                        allIndependent = false;
-                                    }
-                                }
-                                if (allIndependent)
-                                {
-                                    nset.Add(activities[k]);
-                                    independentSets.Add(new HashSet<string>(nset));
-                                }
-                            }
-                        }
+                        nset.Add(activities[j]);
                     }
                 }
+
+                independentSets.UnionWith(GenerateIndependentPowerSet(nset, relations, activities));
             }
             return independentSets;
         }
 
         /// <summary>
-        /// Finds such pairs of maximal independent sets (A, B), so that any activity from A can directly precede any activity from B.
+        /// Checks whether any activity from setA can precede any activity from setB.
         /// </summary>
-        /// <param name="independentSets">A set of independent sets of activities.</param>
+        /// <param name="setA">Set A of independent activities.</param>
+        /// <param name="setB">Set B of independent activities.</param>
         /// <param name="relations">Examined relation matrix (causal footprint).</param>
-        /// <param name="activityIndices">Indexer containing mapping of activities to </param>
-        /// <returns></returns>
-        public static HashSet<Tuple<HashSet<string>, HashSet<string>>> FindMaximalIndependentSetsAB(HashSet<HashSet<string>> independentSets, 
-            Relation[,] relations, 
+        /// <param name="activityIndices">Indexer containing mapping of activities to indices in relation matrix.</param>
+        /// <returns>True if any activity from setA can precede any activity from setB. Otherwise returns false.</returns>
+        private static bool IsValidSetAB(HashSet<string> setA, HashSet<string> setB,
+        Relation[,] relations, Dictionary<string, int> activityIndices)
+        {
+            foreach (string activityA in setA)
+            {
+                foreach (string activityB in setB)
+                {
+                    if (relations[activityIndices[activityA], activityIndices[activityB]] != Relation.Succession)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Generates initial basic independent set (A, B) pairings from 1-activity sets
+        /// (in Alpha algorithm, all activities should be independent on themselves).
+        /// </summary>
+        /// <param name="relations">Examined relation matrix (causal footprint).</param>
+        /// <param name="activityIndices">Indexer containing mapping of activities to indices in relation matrix.</param>
+        /// <returns>Initial 1-activity set pairings of independent sets (A, B)</returns>
+        private static HashSet<Tuple<HashSet<string>, HashSet<string>>> InitializeIndependentSetsAB(
+            Relation[,] relations,
             Dictionary<string, int> activityIndices)
         {
             var setsAB = new HashSet<Tuple<HashSet<string>, HashSet<string>>>();
+
+            foreach (var actA in activityIndices)
+            {
+                foreach (var actB in activityIndices)
+                {
+                    if (relations[actA.Value, actB.Value] == Relation.Succession)
+                    {
+                        setsAB.Add(new Tuple<HashSet<string>, HashSet<string>>(
+                            new HashSet<string>() { actA.Key },
+                            new HashSet<string>() { actB.Key }));
+                    }
+                }
+            }
+
+            return setsAB;
+        }
+
+        /// <summary>
+        /// Finds pairs of maximal independent sets (A, B), such that any activity from A can directly precede any activity from B.
+        /// </summary>
+        /// <param name="independentSets">A set of independent sets of activities.</param>
+        /// <param name="relations">Examined relation matrix (causal footprint).</param>
+        /// <param name="activityIndices">Indexer containing mapping of activities to indices in relation matrix.</param>
+        /// <returns>Maximal independent sets (A, B).</returns>
+        public static HashSet<Tuple<HashSet<string>, HashSet<string>>> FindMaximalIndependentSetsAB(
+            HashSet<HashSet<string>> independentSets, 
+            Relation[,] relations, 
+            Dictionary<string, int> activityIndices)
+        {
+            var setsAB = InitializeIndependentSetsAB(relations, activityIndices);
 
             foreach (HashSet<string> setA in independentSets)
             {
                 foreach (HashSet<string> setB in independentSets)
                 {
-                    bool isValidSet = true;
-                    foreach (string activityA in setA)
-                    {
-                        foreach (string activityB in setB)
-                        {
-                            if (relations[activityIndices[activityA], activityIndices[activityB]] != Relation.Succession)
-                            {
-                                isValidSet = false;
-                            }
-                        }
-                    }
-
-                    if (isValidSet)
+                    if (IsValidSetAB(setA, setB, relations, activityIndices))
                     {
                         bool isBiggerSet = false;
                         var setsToRemove = new HashSet<Tuple<HashSet<string>, HashSet<string>>>();
@@ -100,11 +180,6 @@ namespace ProcessM.NET.Discovery.Alpha
                             }
                         }
                         setsAB.ExceptWith(setsToRemove);
-
-                        if (!isBiggerSet && setA.Count == 1 && setB.Count == 1)
-                        {
-                            isBiggerSet = true;
-                        }
 
                         if (isBiggerSet)
                         {
---
+++ ProcessM.NET/Generator/TraceGenerator.cs
@@ -0,0 +1,167 @@
+﻿using ProcessM.NET.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessM.NET.Generator
+{
+    /// <summary>
+    /// Handler to a simple recursive generator of traces from given Petri net of given maximum length.
+    /// </summary>
+    public class TraceGenerator
+    {
+        /// <summary>
+        /// Petri net from which traces should be generated.
+        /// </summary>
+        private IPetriNet Net { get; }
+
+        /// <summary>
+        /// Maximum length of generated traces (necessary for Petri nets with cycles).
+        /// </summary>
+        private int MaxLength { get; }
+
+        /// <summary>
+        /// Looks for all transitions which can fire in the Petri net given a certain marking of places with tokens.
+        /// </summary>
+        /// <param name="marking">Token marking of places of the Petri net.</param>
+        /// <returns>List of all transitions which can fire in given marking.</returns>
+        private List<ITransition> CanFire(int[] marking)
+        {
+            var fireableTransitions = new List<ITransition>();
+
+            foreach (ITransition transition in Net.Transitions)
+            {
+                bool allMarked = true;
+                foreach (IPlace inputPlace in transition.InputPlaces)
+                {
+                    if (marking[Net.Places.IndexOf(inputPlace)] == 0)
+                    {
+                        allMarked = false;
+                        break;
+                    }
+                }
+
+                if (allMarked)
+                {
+                    fireableTransitions.Add(transition);
+                }
+            }
+
+            return fireableTransitions;
+        }
+
+        /// <summary>
+        /// Calculates a new marking from an old one by subtracting tokens from given input places 
+        /// and incrementing tokens in given output places.
+        /// </summary>
+        /// <param name="inputPlaces">Enumerable collection of input places.</param>
+        /// <param name="outputPlaces">Enumerable collection of output places.</param>
+        /// <param name="originalMarking">Original token marking of places in the Petri net.</param>
+        /// <returns></returns>
+        private int[] RecalculateMarking(IEnumerable<IPlace> inputPlaces, IEnumerable<IPlace> outputPlaces, 
+            int[] originalMarking)
+        {
+            int[] newMarking = new int[originalMarking.Length];
+            originalMarking.CopyTo(newMarking, 0);
+            
+            foreach (IPlace ip in inputPlaces)
+            {
+                newMarking[Net.Places.IndexOf(ip)]--;
+            }
+
+            foreach (IPlace op in outputPlaces)
+            {
+                newMarking[Net.Places.IndexOf(op)]++;
+            }
+
+            return newMarking;
+        }
+
+        /// <summary>
+        /// Checks whether a given marking is an empty marking (contains tokens only in end place).
+        /// </summary>
+        /// <param name="marking">Token marking of places in the Petri net.</param>
+        /// <returns>True if given marking is empty. Otherwise returns false.</returns>
+        private bool MarkingIsEmpty(int[] marking)
+        {
+            int endPlaceIndex = Net.Places.IndexOf(Net.EndPlace);
+
+            for (int i = 0; i < marking.Length; ++i)
+            {
+                if (i == endPlaceIndex)
+                {
+                    continue;
+                }
+                if (marking[i] > 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// "Fires" given transition - adds its activity to a corresponding trace a accordingly reassigns tokens in the Petri net.
+        /// It then recursively "fires" all transitions which can be fired in the new marking.
+        /// If a trace reaches its end (marking becomes empty) or if a length limit is reached or the marking becomes invalid, 
+        /// the recursion is stopped.
+        /// All valid traces are continuously being added to a set of traces.
+        /// </summary>
+        /// <param name="transition">Transition which should be fired in this step.</param>
+        /// <param name="marking">Token marking of places in the Petri net in this step.</param>
+        /// <param name="currentTrace">Trace of activities which have been accumulated so far.</param>
+        /// <param name="traces">A set of valid, finished traces.</param>
+        private void Fire(ITransition transition, int[] marking, 
+            List<string> currentTrace, ref HashSet<List<string>> traces)
+        {
+            currentTrace.Add(transition.Activity);
+            int[] newMarking = RecalculateMarking(transition.InputPlaces, transition.OutputPlaces, marking);
+
+            List<ITransition> fireable = CanFire(newMarking);
+
+            if (currentTrace.Count > MaxLength || (fireable.Count == 0 && !MarkingIsEmpty(newMarking)))
+            {
+                return;
+            }
+            
+            if (MarkingIsEmpty(newMarking))
+            {
+                traces.Add(currentTrace);
+                return;
+            }
+
+            foreach (ITransition t in fireable)
+            {
+                int[] markingCopy = new int[newMarking.Length];
+                newMarking.CopyTo(markingCopy, 0);
+                Fire(t, markingCopy, new List<string>(currentTrace), ref traces);
+            }
+        }
+
+        /// <summary>
+        /// Recursively generates possible traces of given Petri net and of given maximum length.
+        /// </summary>
+        /// <returns>A set of valid, finished traces of activities of given maximum length.</returns>
+        public HashSet<List<string>> GenerateTraces()
+        {
+            var traces = new HashSet<List<string>>();
+
+            int[] initialMarking = new int[Net.Places.Count];
+            initialMarking[Net.Places.IndexOf(Net.StartPlace)] = 1;
+
+            foreach (ITransition transition in CanFire(initialMarking))
+            {
+                Fire(transition, initialMarking, new List<string>(), ref traces);
+            }
+
+            return traces;
+        }
+
+        public TraceGenerator(IPetriNet net, int maxLength = 10)
+        {
+            Net = net;
+            MaxLength = maxLength;
+        }
+    }
+}
