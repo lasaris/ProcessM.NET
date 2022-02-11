fbce4386a267a5b83a8a9310dee50b4ad11f4cda
Otakar Hirš <otakar.hirs@gmail.com>
2022-02-11 12:08:16 +0100

    feat: add optional simplified export

--- ProcessM.NET/Export/DOTExport.cs
+++ ProcessM.NET/Export/DOTExport.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ProcessM.NET.Model.DisplayPetriNet;
 
 namespace ProcessM.NET.Export
 {
     /// <summary>
     /// This is a Petri Net to .DOT file export tool. DOT files can be read and displayed by software such as Graphviz, thus providing
-    /// visialization options of Petri Nets.
+    /// visualization options of Petri Nets.
     /// </summary>
     public static class DOTExport
     {
@@ -43,7 +44,7 @@ namespace ProcessM.NET.Export
         {
             string doubleIndent = indentation + indentation;
             return indentation + "subgraph transitions {\n" +
-                doubleIndent + "node [shape = rect, height = 0.2, width = 2];\n";
+                doubleIndent + "node [class = transition, shape = rect, style=filled, fillcolor = white, height = 0.1, width = 1, fontname=\"times-bold\"];\n";
         }
         
         private static string GetInvisibleTransitionsHeader(string indentation)
@@ -59,7 +60,7 @@ namespace ProcessM.NET.Export
         /// <param name="places">A collection of places in exported Petri Net.</param>
         /// <param name="indentation">Indentation symbol, this class uses "\t" as default.</param>
         /// <returns>A StringBuilder containing a string .DOT representation of places in exported Petri Net.</returns>
-        private static StringBuilder GetPlaces(IEnumerable<IPlace> places, string indentation)
+        private static StringBuilder GetPlaces(IEnumerable<DPlace> places, string indentation)
         {
             string doubleIndent = indentation + indentation;
             StringBuilder outStr = new StringBuilder("");
@@ -78,20 +79,20 @@ namespace ProcessM.NET.Export
         /// <param name="transitions">A collection of transitions in exported Petri Net.</param>
         /// <param name="indentation">Indentation symbol, this class uses "\t" as default.</param>
         /// <returns>A StringBuilder containing a string .DOT representation of transitions in exported Petri Net.</returns>
-        private static StringBuilder GetTransitions(IEnumerable<ITransition> transitions, string indentation)
+        private static StringBuilder GetTransitions(IEnumerable<DTransition> transitions, string indentation)
         {
             string doubleIndent = indentation + indentation;
             StringBuilder outStr = new StringBuilder("");
             outStr.Append(GetTransitionsHeader(indentation));
-            foreach (ITransition t in transitions.Where(tr => !tr.Invisible))
+            foreach (DTransition t in transitions.Where(tr => !tr.Invisible))
             {
-                outStr.Append(doubleIndent + "\"" + t.Activity + "\";\n");
+                outStr.Append(doubleIndent + "\"" + t.Activity + "\" [id=" + t.Id + ",fillcolor=" + t.Color + "];\n");
             }
             outStr.Append(indentation + "}\n");
             return outStr;
         }
 
-        private static StringBuilder GetInvisibleTransitions(IEnumerable<ITransition> transitions, string indentation)
+        private static StringBuilder GetInvisibleTransitions(IEnumerable<DTransition> transitions, string indentation)
         {
             string doubleIndent = indentation + indentation;
             StringBuilder outStr = new StringBuilder("");
@@ -103,16 +104,17 @@ namespace ProcessM.NET.Export
             outStr.Append(indentation + "}\n");
             return outStr;
         }
+        
         /// <summary>
         /// Fills the .DOT graph with arc representation based on transitions of exported Petri Net.
         /// </summary>
         /// <param name="transitions">A collection of transitions in exported Petri Net.</param>
         /// <param name="indentation">Indentation symbol, this class uses "\t" as default.</param>
         /// <returns>A StringBuilder containing a string .DOT representation of arcs in exported Petri Net.</returns>
-        private static StringBuilder GetArcs(List<ITransition> transitions, string indentation)
+        private static StringBuilder GetArcs(List<DTransition> transitions, string indentation)
         {
             StringBuilder outStr = new StringBuilder("");
-            foreach (ITransition t in transitions)
+            foreach (DTransition t in transitions)
             {
                 foreach (IPlace ip in t.InputPlaces)
                 {
@@ -127,20 +129,65 @@ namespace ProcessM.NET.Export
             return outStr;
         }
 
+        private static StringBuilder GetSimplifiedArcs(List<DTransition> transitions, string indentation)
+        {
+            StringBuilder outStr = new StringBuilder("");
+            foreach (DTransition inT in transitions.Where(t => !t.Invisible))
+            {
+                var outTransitions = FindOutTransitions(inT, transitions).Distinct();
+                foreach (var outT in outTransitions)
+                {
+                    outStr.Append(indentation + "\"" + inT.Activity + "\" -> \"" + outT.Activity + "\";\n");
+                }
+            }
+            outStr.Append("}");
+            return outStr;
+        }
+
+        private static IEnumerable<DTransition> FindOutTransitions(DTransition inT, List<DTransition> transitions)
+        {
+            var outTransitions = new List<DTransition>();
+            foreach (var p in inT.OutputPlaces)
+            {
+                foreach (var t in transitions.Where(t => t.InputPlaces.Contains(p)))
+                {
+                    if (t.Invisible)
+                    {
+                        outTransitions.AddRange(FindOutTransitions(t, transitions));
+                    }
+                    else
+                    {
+                        outTransitions.Add(t);
+                    }
+                }
+            }
+            return outTransitions;
+        }
+
+
         /// <summary>
         /// Serializes given Petri Net compliant with IPetriNet interface to a .DOT file string.
         /// </summary>
-        /// <param name="net">An IPetriNet compliant Petri Net.</param>
+        /// <param name="sourceNet">An IPetriNet compliant Petri Net.</param>
+        /// <param name="simplified">Export simplified graph only</param>
         /// <param name="indentation">Indentation symbol, this class uses "\t" as default.</param>
         /// <returns>.dot file string</returns>
-        public static string Serialize(IPetriNet net, string indentation = "\t")
+        public static string Serialize(IPetriNet sourceNet, bool simplified = false, string indentation = "\t")
         {
+            var net = sourceNet is DPetriNet ? (DPetriNet)sourceNet : new DPetriNet(sourceNet);
             StringBuilder outStr = new StringBuilder("");
             outStr.Append(GetGraphHeader());
-            outStr.Append(GetPlaces(net.Places, indentation));
             outStr.Append(GetTransitions(net.Transitions, indentation));
-            outStr.Append(GetInvisibleTransitions(net.Transitions, indentation));
-            outStr.Append(GetArcs(net.Transitions, indentation));
+            if (simplified)
+            {
+                outStr.Append(GetSimplifiedArcs(net.Transitions, indentation));
+            }
+            else
+            {
+                outStr.Append(GetPlaces(net.Places, indentation));
+                outStr.Append(GetInvisibleTransitions(net.Transitions, indentation));
+                outStr.Append(GetArcs(net.Transitions, indentation));
+            }
             return outStr.ToString();
         }
     }
