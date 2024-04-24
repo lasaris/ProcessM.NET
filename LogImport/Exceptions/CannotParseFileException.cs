8239e68ef42ac0581307228aa12dbff192b9c300
Filip Kovac <Filip.Kovac@adacta-fintech.com>
2024-04-24 21:40:19 +0200

    chore: Added docs

--- LogImport/Exceptions/CannotParseFileException.cs
+++ LogImport/Exceptions/CannotParseFileException.cs
@@ -1,8 +1,11 @@
 namespace LogImport.Exceptions
 {
+    /// <summary>
+    ///   Exception thrown when a file cannot be parsed.
+    /// </summary>
     public class CannotParseFileException : Exception
     {
-        public CannotParseFileException() { }
+        public CannotParseFileException() : base() { }
 
         public CannotParseFileException(string message) : base(message) { }
     }
--- LogImport/Interfaces/ILogImporter.cs
+++ LogImport/Interfaces/ILogImporter.cs
@@ -2,9 +2,22 @@ using LogImport.Models;
 
 namespace LogImport.Interfaces;
 
+/// <summary>
+///   Interface for importing event logs
+/// </summary>
 public interface ILogImporter
 {
+    /// <summary>
+    ///     Method containing the logic of loading an event log from a file represented by a path parameter
+    /// </summary>
+    /// <param name="filePath">Path to the file, where log is stored</param>
+    /// <returns><see cref="ImportedEventLog"/></returns>
     ImportedEventLog LoadLog(string filePath);
 
+    /// <summary>
+    ///    Method containing the logic of loading an event log from a stream
+    /// </summary>
+    /// <param name="stream">Opened stream, from which the method can read</param>
+    /// <returns><see cref="ImportedEventLog"/></returns>
     ImportedEventLog LoadLog(Stream stream);
 }
