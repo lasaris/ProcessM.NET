65890e5c920d8fc15246d791cf757d6b51e9d7d8
Filip Kovac <xkovac9@fi.muni.cz>
2024-07-09 18:54:22 +0200

    shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button'
 import './App.css'
 
 function App() {
-    return <>Hello Process Mining</>
+    return (
+        <>
+            Hello Process Mining
+            <Button>Hello there</Button>
+        </>
+    )
 }
 
 export default App
