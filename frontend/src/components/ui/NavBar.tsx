91a22835f61b298fef9bd42b1de15ffbda750dea
Filip Kovac <xkovac9@fi.muni.cz>
2024-10-18 13:35:01 +0200

    fix: updated navbar styling

--- frontend/src/components/ui/NavBar.tsx
+++ frontend/src/components/ui/NavBar.tsx
@@ -22,7 +22,7 @@ export const NavBar: React.FC = () => {
         <nav className="sticky top-0 z-20 bg-white flex flex-row min-h-16 items-center justify-between gap-3 px-4 border-b">
             <h2
                 onClick={navigateHome}
-                className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 hover:cursor-pointer"
+                className="scroll-m-20 text-2xl font-bold tracking-tight hover:cursor-pointer hover:text-blue-600 transition-colors duration-300"
             >
                 ProcessM.NET
             </h2>
