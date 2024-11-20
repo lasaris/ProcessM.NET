a02b752b3f92dd10ca7dbb37992a847a6496ebbb
Filip Kovac <Filip.Kovac@adacta-fintech.com>
2024-11-20 20:19:52 +0100

    fixed api port

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5217';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
