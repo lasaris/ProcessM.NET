05c0854e09fc4bc2e98a317857af32c500e2d3ba
Filip Kovac <Filip.Kovac@adacta-fintech.com>
2024-11-23 17:39:07 +0100

    testing railway config ports

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://api:8000';
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
